bda2d2b766dafbaa9834245cda1e36bbe967b8b9
GAIUS8 <gaius827@gmail.com>
2017-06-25 19:01:07 +0900

    Create Model - POST

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -55,7 +55,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
+    'django_extensions',
+
+    'post',
+    'member',
 ]
 
 MIDDLEWARE = [
--- django_app/post/models.py
+++ django_app/post/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = settings.AUTH_USER_MODEL
+    photo = models.ImageField(
+        upload_to='post',
+        blank=True,
+        null=True,
+    )
+    comment = models.CharField(max_length=100)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
